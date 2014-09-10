91208c99de9575968221b8fb37d62ab1a078f1d9
ashleycoker <ashley.coker@prowebsoftware.net>
2014-09-10 14:18:43 +0100

    added ", mount_options: ["dmode=777,fmode=777"]"
    This got around the issue of an fopen() permission denied issue when using Assets module to upload files.  The images were being set in Google Cloud storage as 0 bytes as line 22 of fileuploader.php was failing to create a file.

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -30,7 +30,7 @@ class Homestead
 
     # Register All Of The Configured Shared Folders
     settings["folders"].each do |folder|
-      config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
+      config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil, mount_options: ["dmode=777,fmode=777"]
     end
 
     # Install All The Configured Nginx Sites
